76d1628929ae52406580c764b26d08d7117783fc
RodrigoValdivia <rodrigo.valdivia@usach.cl>
2024-02-23 22:43:21 -0300

    tres secciones listas

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,12 +12,43 @@ class Multimedia {
 class Reproductor extends Multimedia {
     constructor (url, id){
         super(url)
-        this.id
+        this._id = id
+    }
+    get id(){
+        return this._id
     }
     playMultimedia(){
-
+        modulo.agregarVideoPublico(this._url, this._id)
     }
     setInicio (tiempo){
-        
+        this.id.setAttribute('src', `${this.url}?start=${tiempo}`);
     }
 }
+
+let modulo = (() => {
+    let agregarVideoPrivado = (url,id) => {
+    let element = document.getElementById(id)
+    element.setAttribute('src', url);
+}
+return {
+    agregarVideoPublico: (url, id)=> {
+        agregarVideoPrivado(url, id)
+    },
+};
+})();
+
+let peliculas = new Reproductor ('https://www.youtube.com/embed/gdE--uAySvs?si=DPl7qeAwUHNDKA37', 'peliculas');
+
+let series = new Reproductor ('https://www.youtube.com/embed/8TO7BW9LSb8?si=Pa1ouysn8_Gu5YBf', 'series');
+
+let musica = new Reproductor ('https://www.youtube.com/embed/JN24wVxae_M?si=rTNzUGrH7rH-ZHfA', 'musica');
+
+peliculas.playMultimedia();
+series.playMultimedia();
+musica.playMultimedia();
+
+//Llamar al setinicio
+
+peliculas.setInicio(10);
+series.setInicio(15);
+musica.setInicio(10);
